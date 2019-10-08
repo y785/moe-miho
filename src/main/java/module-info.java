5f863e8bdcd95e77bdc83b39be2d689faf634f5e
y785 <j7852398521@gmail.com>
2019-10-08 18:56:45 -0500

    Added module-info.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module moe.maple.miho {
+    exports moe.maple.miho.foothold;
+    exports moe.maple.miho.line;
+    exports moe.maple.miho.point;
+    exports moe.maple.miho.rect;
+    exports moe.maple.miho.space;
+    exports moe.maple.miho.tree;
+    exports moe.maple.miho.util;
+
+    exports moe.maple.miho.space.bst;
+    exports moe.maple.miho.tree.bst;
+}
